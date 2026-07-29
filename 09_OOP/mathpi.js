b5e01905c79f08e364e5360de09ba8d17ec9e6e5
Shubham Raj <sprashar889@gmail.com>
2026-07-29 08:58:51 +0530

    Iteration case updated

--- 09_OOP/mathpi.js
+++ 09_OOP/mathpi.js
@@ -1,7 +1,6 @@
 const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
 console.log(descriptor);
 
-
 console.log(Math.PI);
 Math.PI = 5;
 console.log(Math.PI);
@@ -9,17 +8,24 @@ console.log(Math.PI);
 // PROPERTY DESCRIPTOR
 
 const chai = {
-    name: "chai",
-    price: 200,
-    isAvailable: true
+  name: "chai",
+  price: 200,
+  isAvailable: true,
+  orderChai: function () {
+    console.log(`Code fateeee giiii`);
+  },
+};
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+// Object.defineProperty(chai, "name", {
+//   writable: false,
+//   enumerable: false,
+// });
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// Let's iterate the object chai....
+for (let [key, value] of Object.entries(chai)) {
+  if (typeof value !== "function") {
+    console.log(`${key} : ${value}`);
+  }
 }
-
-console.log(Object.getOwnPropertyDescriptor(chai, "name"));
-Object.defineProperty(chai, "name", {
-    writable: false,
-    enumerable: false
-});
-console.log(Object.getOwnPropertyDescriptor(chai, "name"));
-
-
-
